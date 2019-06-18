ead866a8a9467a9a66a61a290a3ad96710f66539
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-18 18:52:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,7 @@
 def countdown(count)
     if count == 0
       puts "HAPPY NEW YEAR!"
-     else
-       loop do
-     until count == 0
+    else until count == 0
      puts "#{count} SECOND(S)!"
      count -= 1
     end
